b55d78f47e28a21f94dcd06d2a8d05fc1af17ac0
Jolyon Conway <127051917+Jolyon-Conway@users.noreply.github.com>
2023-12-15 20:24:08 +0000

    finished

--- sketch.js
+++ sketch.js
@@ -9,6 +9,8 @@ let enemy1;
 let enemy2;
 let enemy3;
 let score = 0;
+let enemyCount;
+let bossArray = [];
 function setup() {
     createCanvas(1480, 700);
     player = new player(740, 680, 5);
@@ -23,6 +25,7 @@ function preload() {
     enemy1 = loadImage("Images/enemy1.png");
     enemy2 = loadImage("Images/enemy2.png");
     enemy3 = loadImage("Images/enemy3.png");
+    bossSprite = loadImage("Images/boss.png");
 }
 
 function draw() {
@@ -48,17 +51,17 @@ function draw() {
         }
         //bullets
         if (bullets.length > 0) {
-            for (let i = 0; i < bullets.length; i++) {
+            for (let i = bullets.length -1; i >= 0; i--) {
                 bullets[i].move();
-                if (bullets.length > 0) {
+                if (bullets[i]) {
                     bullets[i].display();
                 }
             }
         }
         //shields
-        for (i = 0; i < shields.length; i++) {
-            for (j = 0; j < shields[i].length; j++) {
-                for (k = 0; k < shields[i][j].length; k++) {
+        for (i = shields.length-1; i >= 0 ; i--) {
+            for (j = shields[i].length -1 ; j >= 0; j--) {
+                for (k = shields[i][j].length-1; k >= 0; k--) {
                     shields[i][j][k].display();
                     shields[i][j][k].hitCheck();
                 }
@@ -75,11 +78,31 @@ function draw() {
                 enemies[i][j].move();
                 if (enemies[i][j].hitCheck()) {
                     enemies[i].splice(j, 1);
+                    enemyCount -= 1;
                 } else {
                     enemies[i][j].fire();
                 }
             }
         }
+        if (enemyCount == 0) {
+            spawnEnemies();
+        }
+        //boss
+        if (Math.floor(Math.random() * 1000) == 476) {
+            if (Math.floor(Math.random() * 2) == 0) {
+                bossArray.push(new boss(-200, 50, "Right"));
+            } else {
+                bossArray.push(new boss(1680, 50, "Left"));
+            }
+        }
+        for (i = bossArray.length -1; i >= 0; i--) {
+            bossArray[i].display();
+            bossArray[i].move();
+            if (bossArray[i].hitCheck()) {
+                bossArray.splice(i, 1);
+            }
+        }
+        //frame
         frame = (frame + 1) % 1000;
     } else {
         textSize(32);
@@ -95,6 +118,7 @@ function draw() {
         triangle(730, 252.5, 760, 270, 730, 287.5);
         if (mouseIsPressed && mouseX > 725 && mouseX < 765 && mouseY > 250 && mouseY < 290) {
             player.revive();
+            score = 0;
             alive = true;
         }
     }
@@ -151,76 +175,20 @@ function resetShields() {
             [new shield(1320, 590, 3)],[new shield(1320, 610, 3)],
             [new shield(1301, 590, 3)],[new shield(1301, 610, 3)],[new shield(1301, 630, 3)],
     ], 
-
-
     ]
 }
 
-
-function setHighScore() {
-    if (score > highScore) {
-        highScore = score;
-        localStorage.setItem("highScore", highScore);
-    }
-}
-
-function resetShields() {
-    shields = [
-        [
-            [new shield(158, 590, 3)],[new shield(158, 610, 3)],[new shield(158, 630, 3)],
-            [new shield(139, 590, 3)],[new shield(139, 610, 3)],
-            [new shield(120, 590, 3)],[new shield(120, 610, 3)],
-            [new shield(101, 590, 3)],[new shield(101, 610, 3)],[new shield(101, 630, 3)],
-    ],
-        [
-            [new shield(358, 590, 3)],[new shield(358, 610, 3)],[new shield(358, 630, 3)],
-            [new shield(339, 590, 3)],[new shield(339, 610, 3)],
-            [new shield(320, 590, 3)],[new shield(320, 610, 3)],
-            [new shield(301, 590, 3)],[new shield(301, 610, 3)],[new shield(301, 630, 3)],
-    ],
-        [
-            [new shield(558, 590, 3)],[new shield(558, 610, 3)],[new shield(558, 630, 3)],
-            [new shield(539, 590, 3)],[new shield(539, 610, 3)],
-            [new shield(520, 590, 3)],[new shield(520, 610, 3)],
-            [new shield(501, 590, 3)],[new shield(501, 610, 3)],[new shield(501, 630, 3)],
-    ],
-        [
-            [new shield(758, 590, 3)],[new shield(758, 610, 3)],[new shield(758, 630, 3)],
-            [new shield(739, 590, 3)],[new shield(739, 610, 3)],
-            [new shield(720, 590, 3)],[new shield(720, 610, 3)],
-            [new shield(701, 590, 3)],[new shield(701, 610, 3)],[new shield(701, 630, 3)],
-    ],
-        [
-            [new shield(958, 590, 3)],[new shield(958, 610, 3)],[new shield(958, 630, 3)],
-            [new shield(939, 590, 3)],[new shield(939, 610, 3)],
-            [new shield(920, 590, 3)],[new shield(920, 610, 3)],
-            [new shield(901, 590, 3)],[new shield(901, 610, 3)],[new shield(901, 630, 3)],
-    ],
-        [
-            [new shield(1158, 590, 3)],[new shield(1158, 610, 3)],[new shield(1158, 630, 3)],
-            [new shield(1139, 590, 3)],[new shield(1139, 610, 3)],
-            [new shield(1120, 590, 3)],[new shield(1120, 610, 3)],
-            [new shield(1101, 590, 3)],[new shield(1101, 610, 3)],[new shield(1101, 630, 3)],
-    ], 
-        [
-            [new shield(1358, 590, 3)],[new shield(1358, 610, 3)],[new shield(1358, 630, 3)],
-            [new shield(1339, 590, 3)],[new shield(1339, 610, 3)],
-            [new shield(1320, 590, 3)],[new shield(1320, 610, 3)],
-            [new shield(1301, 590, 3)],[new shield(1301, 610, 3)],[new shield(1301, 630, 3)],
-    ], 
-
-
-    ]
-}
 
 function spawnEnemies() {
     enemies = [
-        [new enemy(50, 100, 1)], [new enemy(120, 100, 1)], [new enemy(190, 100, 1)], [new enemy(260, 100, 1)], [new enemy(330, 100, 1)], [new enemy(400, 100, 1)], [new enemy(470, 100, 1)], [new enemy(540, 100, 1)], [new enemy(610, 100, 1)], [new enemy(680, 100, 1)], [new enemy(750, 100, 1)], [new enemy(820, 100, 1)], [new enemy(890, 100, 1)], [new enemy(960, 100, 1)], [new enemy(1030, 100, 1)], [new enemy(1100, 100, 1)], [new enemy(1170, 100, 1)], [new enemy(1240, 100, 1)],
-        [new enemy(50, 160, 1)], [new enemy(120, 160, 1)], [new enemy(190, 160, 1)], [new enemy(260, 160, 1)], [new enemy(330, 160, 1)], [new enemy(400, 160, 1)], [new enemy(470, 160, 1)], [new enemy(540, 160, 1)], [new enemy(610, 160, 1)], [new enemy(680, 160, 1)], [new enemy(750, 160, 1)], [new enemy(820, 160, 1)], [new enemy(890, 160, 1)], [new enemy(960, 160, 1)], [new enemy(1030, 160, 1)], [new enemy(1100, 160, 1)], [new enemy(1170, 160, 1)], [new enemy(1240, 160, 1)],
-        [new enemy(50, 220, 1)], [new enemy(120, 220, 1)], [new enemy(190, 220, 1)], [new enemy(260, 220, 1)], [new enemy(330, 220, 1)], [new enemy(400, 220, 1)], [new enemy(470, 220, 1)], [new enemy(540, 220, 1)], [new enemy(610, 220, 1)], [new enemy(680, 220, 1)], [new enemy(750, 220, 1)], [new enemy(820, 220, 1)], [new enemy(890, 220, 1)], [new enemy(960, 220, 1)], [new enemy(1030, 220, 1)], [new enemy(1100, 220, 1)], [new enemy(1170, 220, 1)], [new enemy(1240, 220, 1)],
-        [new enemy(50, 280, 1)], [new enemy(120, 280, 1)], [new enemy(190, 280, 1)], [new enemy(260, 280, 1)], [new enemy(330, 280, 1)], [new enemy(400, 280, 1)], [new enemy(470, 280, 1)], [new enemy(540, 280, 1)], [new enemy(610, 280, 1)], [new enemy(680, 280, 1)], [new enemy(750, 280, 1)], [new enemy(820, 280, 1)], [new enemy(890, 280, 1)], [new enemy(960, 280, 1)], [new enemy(1030, 280, 1)], [new enemy(1100, 280, 1)], [new enemy(1170, 280, 1)], [new enemy(1240, 280, 1)],
-        [new enemy(50, 340, 1)], [new enemy(120, 340, 1)], [new enemy(190, 340, 1)], [new enemy(260, 340, 1)], [new enemy(330, 340, 1)], [new enemy(400, 340, 1)], [new enemy(470, 340, 1)], [new enemy(540, 340, 1)], [new enemy(610, 340, 1)], [new enemy(680, 340, 1)], [new enemy(750, 340, 1)], [new enemy(820, 340, 1)], [new enemy(890, 340, 1)], [new enemy(960, 340, 1)], [new enemy(1030, 340, 1)], [new enemy(1100, 340, 1)], [new enemy(1170, 340, 1)], [new enemy(1240, 340, 1)],
+        [new enemy(50, 100, 1)], [new enemy(120, 100, 1)], [new enemy(190, 100, 1)], [new enemy(260, 100, 1)], [new enemy(330, 100, 1)], [new enemy(400, 100, 1)], [new enemy(470, 100, 1)], [new enemy(540, 100, 1)], [new enemy(610, 100, 1)], [new enemy(680, 100, 1)], [new enemy(750, 100, 1)], [new enemy(820, 100, 1)], [new enemy(890, 100, 1)], [new enemy(960, 100, 1)], [new enemy(1030, 100, 1)], [new enemy(1100, 100, 1)], [new enemy(1170, 100, 1)],
+        [new enemy(50, 160, 2)], [new enemy(120, 160, 2)], [new enemy(190, 160, 2)], [new enemy(260, 160, 2)], [new enemy(330, 160, 2)], [new enemy(400, 160, 2)], [new enemy(470, 160, 2)], [new enemy(540, 160, 2)], [new enemy(610, 160, 2)], [new enemy(680, 160, 2)], [new enemy(750, 160, 2)], [new enemy(820, 160, 2)], [new enemy(890, 160, 2)], [new enemy(960, 160, 2)], [new enemy(1030, 160, 2)], [new enemy(1100, 160, 2)], [new enemy(1170, 160, 2)],
+        [new enemy(50, 220, 2)], [new enemy(120, 220, 2)], [new enemy(190, 220, 2)], [new enemy(260, 220, 2)], [new enemy(330, 220, 2)], [new enemy(400, 220, 2)], [new enemy(470, 220, 2)], [new enemy(540, 220, 2)], [new enemy(610, 220, 2)], [new enemy(680, 220, 2)], [new enemy(750, 220, 2)], [new enemy(820, 220, 2)], [new enemy(890, 220, 2)], [new enemy(960, 220, 2)], [new enemy(1030, 220, 2)], [new enemy(1100, 220, 2)], [new enemy(1170, 220, 2)],
+        [new enemy(50, 280, 3)], [new enemy(120, 280, 3)], [new enemy(190, 280, 3)], [new enemy(260, 280, 3)], [new enemy(330, 280, 3)], [new enemy(400, 280, 3)], [new enemy(470, 280, 3)], [new enemy(540, 280, 3)], [new enemy(610, 280, 3)], [new enemy(680, 280, 3)], [new enemy(750, 280, 3)], [new enemy(820, 280, 3)], [new enemy(890, 280, 3)], [new enemy(960, 280, 3)], [new enemy(1030, 280, 3)], [new enemy(1100, 280, 3)], [new enemy(1170, 280, 3)],
+        [new enemy(50, 340, 3)], [new enemy(120, 340, 3)], [new enemy(190, 340, 3)], [new enemy(260, 340, 3)], [new enemy(330, 340, 3)], [new enemy(400, 340, 3)], [new enemy(470, 340, 3)], [new enemy(540, 340, 3)], [new enemy(610, 340, 3)], [new enemy(680, 340, 3)], [new enemy(750, 340, 3)], [new enemy(820, 340, 3)], [new enemy(890, 340, 3)], [new enemy(960, 340, 3)], [new enemy(1030, 340, 3)], [new enemy(1100, 340, 3)], [new enemy(1170, 340, 3)],
     ]
+    enemyCount = 85;
+    frame = 0;
 }
 // classes
 
@@ -274,6 +242,7 @@ class player {
         this.Ypos = 680;
         this.health = 5;
         resetShields();
+        spawnEnemies();
     }
 }
 
@@ -398,4 +367,36 @@ class enemy {
         }
     }
 
+}
+
+class boss {
+    constructor(newXpos, newYpos, direction) {
+        this.Xpos = newXpos;
+        this.Ypos = newYpos;
+        this.direction = direction;
+    }
+
+    display() {
+        image(bossSprite, this.Xpos, this.Ypos, 137, 40)
+    }
+
+    move() {
+        if (this.direction == "Right") {
+            this.Xpos += 10;
+        } else if (this.direction == "Left") {
+            this.Xpos -=10;
+        }
+    }
+
+    hitCheck() {
+        for (let i = 0; i < bullets.length; i++) {
+            if (bullets[i].type == "player") {
+                if (bullets[i].Xpos > this.Xpos && bullets[i].Xpos < this.Xpos + 137 && bullets[i].Ypos > this.Ypos && bullets[i].Ypos < this.Ypos + 40) {
+                    bullets.splice(i, 1);
+                    score += 200;
+                    return true;
+                }
+            }
+        }
+    }
 }
